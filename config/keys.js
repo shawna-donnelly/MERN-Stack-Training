722c36794dd5b9e41f926412960a0ffd5056fbde
Shawna <shawna.donnelly@nwyc.com>
2019-03-26 20:44:09 -0500

    update keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://shawna:shawna@devconnecttutorial-vntch.mongodb.net/test?retryWrites=true",
-  secretOrKey: "secret"
-};
+if (process.env.node_env === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
