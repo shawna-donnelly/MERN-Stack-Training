1b24d89143ddac14037bd2ff6a218ecdfe48db1e
Shawna <shawna.donnelly@nwyc.com>
2019-03-27 16:37:07 -0500

    sdfsdf

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const passport = require("passport");
 const path = require("path");
 
 const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profiles");
+const post = require("./routes/api/posts");
 
 const app = express();
 
-// Body parser middleware
+//Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -32,16 +32,15 @@ require("./config/passport")(passport);
 // Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use("/api/posts", post);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
+// Serve static assets if in production
+if (process.env.node_env === "production") {
+  // Set  Static folder
   app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 const port = process.env.PORT || 5000;
